4a9b5805f53797089d67d6269766537529bc6321
app00Jul <71953215+app00jul@users.noreply.github.com>
2021-11-29 22:08:32 +0000

    Update HW.js

--- HW.js
+++ HW.js
@@ -1,1 +1,185 @@
+$(function(){
+    /*For CAL103*/
+    $("#CAL103_checkbox").click(function(event){
+        if ($(this).is(":checked")) {
+            $("#CAL103_selected_0").show();
+        }
+        else{
+            $("#CAL103_selected_0").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#CAL103_selected_1").show();
+        }
+        else{
+            $("#CAL103_selected_1").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#CAL103_selected_2").show();
+        }
+        else{
+            $("#CAL103_selected_2").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#CAL103_selected_3").show();
+        }
+        else{
+            $("#CAL103_selected_3").hide();
+        }
+    })
 
+    /*For CS115*/
+    $("#CS115_checkbox").click(function(event){
+        if ($(this).is(":checked")) {
+            $("#CS115_selected_0").show();
+        }
+        else{
+            $("#CS115_selected_0").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#CS115_selected_1").show();
+        }
+        else{
+            $("#CS115_selected_1").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#CS115_selected_2").show();
+        }
+        else{
+            $("#CS115_selected_2").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#CS115_selected_3").show();
+        }
+        else{
+            $("#CS115_selected_3").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#CS115_selected_4").show();
+        }
+        else{
+            $("#CS115_selected_4").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#CS115_selected_5").show();
+        }
+        else{
+            $("#CS115_selected_5").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#CS115_selected_6").show();
+        }
+        else{
+            $("#CS115_selected_6").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#CS115_selected_7").show();
+        }
+        else{
+            $("#CS115_selected_7").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#CS115_selected_8").show();
+        }
+        else{
+            $("#CS115_selected_8").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#CS115_selected_9").show();
+        }
+        else{
+            $("#CS115_selected_9").hide();
+        }
+    }) 
+
+    /*For CS146*/
+    $("#CS146_checkbox").click(function(event){
+        if ($(this).is(":checked")) {
+            $("#CS146_selected_0").show();
+        }
+        else{
+            $("#CS146_selected_0").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#CS146_selected_1").show();
+        }
+        else{
+            $("#CS146_selected_1").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#CS146_selected_2").show();
+        }
+        else{
+            $("#CS146_selected_2").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#CS146_selected_3").show();
+        }
+        else{
+            $("#CS146_selected_3").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#CS146_selected_4").show();
+        }
+        else{
+            $("#CS146_selected_4").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#CS146_selected_5").show();
+        }
+        else{
+            $("#CS146_selected_5").hide();
+        }
+    })   
+
+    /*For PEP111*/
+    $("#PEP111_checkbox").click(function(event){
+        if ($(this).is(":checked")) {
+            $("#PEP111_selected_0").show();
+        }
+        else{
+            $("#PEP111_selected_0").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#PEP111_selected_1").show();
+        }
+        else{
+            $("#PEP111_selected_1").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#PEP111_selected_2").show();
+        }
+        else{
+            $("#PEP111_selected_2").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#PEP111_selected_3").show();
+        }
+        else{
+            $("#PEP111_selected_3").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#PEP111_selected_4").show();
+        }
+        else{
+            $("#PEP111_selected_4").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#PEP111_selected_5").show();
+        }
+        else{
+            $("#PEP111_selected_5").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#PEP111_selected_6").show();
+        }
+        else{
+            $("#PEP111_selected_6").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#PEP111_selected_7").show();
+        }
+        else{
+            $("#PEP111_selected_7").hide();
+        }
+    })   
+})
